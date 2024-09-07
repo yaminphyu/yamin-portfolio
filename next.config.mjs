0fc3c31b528280ff7743bdfb97b7e01a7f004165
yamin <yamin148@infinity.gmail.com>
2024-09-07 18:04:00 +0700

    added navbar

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    PROJECT_NAME: process.env.PROJECT_NAME,
+  },
 };
 
 export default nextConfig;
