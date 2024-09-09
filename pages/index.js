903726332631d7a18df109f7763325bf13b127f9
yamin <yamin148@infinity.gmail.com>
2024-09-09 08:37:19 +0700

    dynamic window dimensiom

--- pages/index.js
+++ pages/index.js
@@ -4,15 +4,24 @@ import { Container } from "@chakra-ui/react";
 import { AR_One_Sans } from "next/font/google";
 import NavBar from "@/components/NavBar";
 import MobileSidebar from "@/components/Mobile/MobileSidebar";
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { MobileSidebarContext } from "@/context/MobileSidebarContext";
+import useWindowDimension from "@/utils/useWindowDimension";
 
 const arOneSans = AR_One_Sans({
   subsets: ["latin"],
 });
 
 export default function Home() {
-  const { toggle } = useContext(MobileSidebarContext);
+  const { toggle, setToggle } = useContext(MobileSidebarContext);
+
+  const { width } = useWindowDimension();
+
+  useEffect(() => {
+    if (width > 768) {
+      setToggle(false);
+    }
+  }, [width]);
   
   return (
     <Container maxW='container.full' className={`${arOneSans.className} w-full h-screen !p-0`}>
