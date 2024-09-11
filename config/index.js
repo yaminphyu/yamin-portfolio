925664d712568f4544734e33bf0a274acff85efb
yamin <yamin148@infinity.gmail.com>
2024-09-11 15:42:39 +0700

    added project section

--- config/index.js
+++ config/index.js
@@ -4,3 +4,27 @@ export const NavBars = [
     {id: 3, name: 'Experiences', url: '#experiences'},
     {id: 4, name: 'Contact', url: '#contact'},
 ];
+
+export const SelfMakedProjects = [
+    {
+        url: 'https://github.com/yaminphyu/yamin-portfolio',
+        name: 'Portfolio Website',
+        description: 'Now you are using this website because it is my first portfolio website.',
+        technology: 'NextJS and Tailwind CSS',
+        year: '2022',
+    },
+    {
+        url: 'https://github.com/yaminphyu/google-recaptcha-with-nextjs',
+        name: 'google-recaptcha-with-nextjs',
+        description: 'It is a Google reCAPTCHA implementation in Next.js. It is not a huge project. I just tested for new thing.',
+        technology: 'NextJS',
+        year: '2022',
+    },
+    {
+        url: 'https://github.com/yaminphyu/country-finder-with-ip',
+        name: 'country-finder-with-ip',
+        description: 'It is a country finder with IP. It is a testing project because of the need of company requirements.',
+        technology: 'NextJS',
+        year: '2022',
+    },
+];
